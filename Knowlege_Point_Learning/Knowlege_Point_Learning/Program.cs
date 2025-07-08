e8f83014e6763e4b7ead86c68a7d6df00a6eed56
hjq <374639959@qq.com>
2025-07-08 15:48:59 +0800

    first commit

---
+++ Knowlege_Point_Learning/Knowlege_Point_Learning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowlege_Point_Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
