4a2d1804ef9739f0b952f6ee97a50f8956d8360e
hjq <374639959@qq.com>
2025-07-08 16:33:02 +0800

    添加prism框架，MahApps.Metro框架

---
+++ DailyApp/DailyApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DailyApp.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DailyApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            // Create the main window of the application
+            return new MainWin
+            {
+                Title = "Daily App",
+                Width = 800,
+                Height = 600
+            };
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+
+}
---
+++ DailyApp/DailyApp/ViewModels/MainWinViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DailyApp.ViewModels
+{
+    internal class MainWinViewModel
+    {
+        public DelegateCommand Click_btn { get; }
+        public MainWinViewModel()
+        {
+            Click_btn = new DelegateCommand(OnClick_btn);
+        }
+
+        private void OnClick_btn()
+        {
+            MessageBox.Show("Hello, World!", "Greeting", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
