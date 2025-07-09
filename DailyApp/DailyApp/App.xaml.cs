8c4c2b1139a662e8e0e5d20df359a0f878a7419c
hjq <374639959@qq.com>
2025-07-09 15:04:24 +0800

    引用MahApps.Metro框架设计

--- DailyApp/DailyApp/App.xaml.cs
+++ DailyApp/DailyApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using DailyApp.Views;
+﻿using ControlzEx.Theming;
+using DailyApp.ViewModels;
+using DailyApp.Views;
 using System.Configuration;
 using System.Data;
 using System.Windows;
@@ -23,7 +25,24 @@ namespace DailyApp
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterDialog<LoginUC, LoginUCViewModel>();
         }
+
+        //重写初始化方法，判断是否登录，若未登录则跳转到登录页面
+        protected override void OnInitialized()
+        {
+            var dialog = Container.Resolve<IDialogService>();
+            dialog.ShowDialog("LoginUC",callback =>
+            {
+                if(callback.Result != ButtonResult.OK)
+                {
+                    Environment.Exit(0);
+                    return;
+                }
+            });
+            base.OnInitialized();
+        }
+
     }
 
 }
