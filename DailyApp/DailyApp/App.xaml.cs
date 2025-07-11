4699adad5382ffa8284ee16ec9df86da88e41385
hjq <374639959@qq.com>
2025-07-11 17:29:04 +0800

    首次提交，已完成登录框基本功能

--- DailyApp/DailyApp/App.xaml.cs
+++ DailyApp/DailyApp/App.xaml.cs
@@ -1,48 +1,39 @@
-﻿using ControlzEx.Theming;
+﻿using System;
+using System.Windows;
 using DailyApp.ViewModels;
 using DailyApp.Views;
-using System.Configuration;
-using System.Data;
-using System.Windows;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
 
 namespace DailyApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : PrismApplication
+    public partial class App
     {
         protected override Window CreateShell()
         {
-            // Create the main window of the application
-            return new MainWin
-            {
-                Title = "Daily App",
-                Width = 800,
-                Height = 600
-            };
+            return Container.Resolve<MainWindow>();
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            containerRegistry.RegisterDialog<LoginUC, LoginUCViewModel>();
+            containerRegistry.RegisterDialog<Login, LoginViewModel>();
         }
 
-        //重写初始化方法，判断是否登录，若未登录则跳转到登录页面
         protected override void OnInitialized()
         {
             var dialog = Container.Resolve<IDialogService>();
-            dialog.ShowDialog("LoginUC",callback =>
+            dialog.ShowDialog("Login", callback =>
             {
-                if(callback.Result != ButtonResult.OK)
+                if (callback.Result != ButtonResult.OK)
                 {
                     Environment.Exit(0);
                     return;
                 }
+                base.OnInitialized();
             });
-            base.OnInitialized();
         }
-
     }
-
 }
---
+++ DailyApp/DailyApp/Extensions/PasswordHelper.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DailyApp.Extensions
+{
+    public class PasswordHelper
+    {
+
+
+        /// <summary>
+        /// 获取密码的静态方法
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string  GetPwd(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PwdProperty);
+        }
+
+        /// <summary>
+        /// 设置密码的静态方法
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetPwd(DependencyObject obj,string value)
+        {
+            obj.SetValue(PwdProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PwdProperty =
+            DependencyProperty.RegisterAttached("Pwd", 
+                typeof(string), 
+                typeof(PasswordHelper), 
+                new PropertyMetadata("", OnPwdChanged));//绑定属性变化事件
+
+        /// <summary>
+        /// 密码变化事件
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        private static void OnPwdChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox passwordBox = d as PasswordBox;
+            if (passwordBox != null && passwordBox.Password != (string)e.NewValue)
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+        }
+    }
+
+    public class PasswordBoxBehavior: Behavior<PasswordBox>
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        override protected void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PasswordChanged += PasswordBox_PasswordChanged;
+        }
+
+        /// <summary>
+        /// 密码变化事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            string password = PasswordHelper.GetPwd(passwordBox);
+            if (passwordBox != null && passwordBox.Password != password)
+            {
+                PasswordHelper.SetPwd(passwordBox, passwordBox.Password);
+            }
+        }
+
+        /// <summary>
+        /// 取消绑定
+        /// </summary>
+        override protected void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PasswordChanged -= PasswordBox_PasswordChanged;
+        }
+    }
+}
