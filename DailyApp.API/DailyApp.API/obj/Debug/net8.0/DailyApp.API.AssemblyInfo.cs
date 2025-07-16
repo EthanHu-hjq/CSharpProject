82371893a593a25fa471115861996a126533adbb
hjq <374639959@qq.com>
2025-07-16 23:09:54 +0800

    WPF调用API准备工作

--- DailyApp.API/DailyApp.API/obj/Debug/net8.0/DailyApp.API.AssemblyInfo.cs
+++ DailyApp.API/DailyApp.API/obj/Debug/net8.0/DailyApp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe717098ba74ea056052dfce2a1960571c117dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfbffd6b90235a20af0b8946dd52eedb603c562")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DailyApp/DailyApp/HttpClients/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyApp.HttpClients
+{
+    /// <summary>
+    /// 接收模型
+    /// </summary>
+    public class ApiResponse
+    {
+        /// <summary>
+        /// 结果编码
+        /// </summary>
+        public int ResultCode { get; set; }
+
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string ResultMessage { get; set; }
+
+        /// <summary>
+        /// 结果数据
+        /// </summary>
+        public object ResultData { get; set; }
+    }
+}
